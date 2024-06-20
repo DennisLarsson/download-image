35cef9b7602f6fbe60d0af4a2991b457f56ad04e
DennisLarsson <dennis.j.larsson@gmail.com>
2024-06-20 09:56:53 +0200

    debugging message

--- download_samples.sh
+++ download_samples.sh
@@ -10,6 +10,7 @@ while IFS= read -r LINE; do
         echo "Error: Sample ${SAMPLE_NAME}.fq.gz not found in samples.json" >&2
         exit 1
     else
+        echo "Downloading ${SAMPLE_NAME}.fq.gz using ID $ID..."
         gdown --id $ID --output "${SAMPLE_NAME}.fq.gz"
     fi
 done < $POPMAP
