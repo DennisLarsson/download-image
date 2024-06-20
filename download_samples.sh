16fbce067898e0071334affbca5ffd35d4975649
DennisLarsson <dennis.j.larsson@gmail.com>
2024-06-20 09:47:30 +0200

    Add new files

---
+++ download_samples.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+SAMPLES_JSON=$0
+POPMAP=$1
+
+while IFS= read -r LINE; do
+    SAMPLE_NAME=$(echo "$LINE" | cut -f1)
+    ID=$(jq -r --arg key "${SAMPLE_NAME}.fq.gz" '.[$key]' $SAMPLES_JSON)
+    if [[ $ID == "null" ]]; then
+        echo "Error: Sample ${SAMPLE_NAME}.fq.gz not found in samples.json" >&2
+        exit 1
+    else
+        gdown $ID --output "${SAMPLE_NAME}.fq.gz"
+    fi
+done < $POPMAP
